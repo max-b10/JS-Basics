00e3da7cfa31aca07cdc61657407fa822eb022d8
max-b10 <bungaymax@gmail.com>
2021-02-01 10:39:51 +0000

    Create boolean.js

---
+++ basics/boolean.js
@@ -0,0 +1,13 @@
+let temp = 32;
+let isFreezing = temp <= 32;
+console.log(isFreezing);
+
+// Create age variabl set to your age
+let age = 50;
+// Calculate isChild - if they're 11 or under.
+let isChild = age <= 11;
+// Calculate isAdult - if they're 18 or over.
+let isAdult = age >= 18;
+// Print child and adult value.
+console.log(isChild);
+console.log(isAdult);
