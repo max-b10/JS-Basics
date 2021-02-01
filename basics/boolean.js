0c0c79c74218471fec60959f2acef2341874c1f9
max-b10 <bungaymax@gmail.com>
2021-02-01 11:07:31 +0000

    Update boolean.js
    More if statements.

--- basics/boolean.js
+++ basics/boolean.js
@@ -9,10 +9,17 @@ if (temp >= 110) {
 
 // Create age variabl set to your age
 let age = 50;
-// Calculate isChild - if they're 11 or under.
-let isChild = age <= 11;
-// Calculate isAdult - if they're 18 or over.
-let isAdult = age >= 18;
-// Print child and adult value.
-console.log(isChild);
-console.log(isAdult);
+// // Calculate isChild - if they're 11 or under.
+// let isChild = age <= 11;
+// // Calculate isAdult - if they're 18 or over.
+// let isAdult = age >= 18;
+// // Print child and adult value.
+// console.log(isChild);
+// console.log(isAdult);
+
+if (age <= 11) {
+  console.log("Youre a child");
+}
+if (age >= 18) {
+  console.log("Youre an adult");
+}
